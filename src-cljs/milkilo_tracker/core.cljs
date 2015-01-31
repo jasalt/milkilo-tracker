1c8bb9f91864ea24ed67d4ebd5a4d8a56d1b4a64
Jarkko Saltiola <jarkko.saltiola@gmail.com>
2015-01-31 14:53:39 +0200

    Add routes. Some basic layout.

--- src-cljs/milkilo_tracker/core.cljs
+++ src-cljs/milkilo_tracker/core.cljs
@@ -2,56 +2,19 @@
   (:require [reagent.core :as reagent :refer [atom]]
             [secretary.core :as secretary]
             [reagent-forms.core :refer [bind-fields]]
-            [ajax.core :refer [POST]])
-  (:require-macros [secretary.core :refer [defroute]]))
+            [ajax.core :refer [POST]]
+            )
+  (:require-macros [secretary.core :refer [defroute]])
+  )
 
-(def state (atom {:saved? false}))
+(def state (atom {:saved? false}
+                 {:entries []}
+                 {:entry-id nil}))
 
 (defn row [label & body]
   [:div.row
    [:div.col-md-2 [:span label]]
    [:div.col-md-3 body]])
-
-(defn text-input [id label]
-  (row label [:input.form-control {:field :text :id id}]))
-
-(defn selection-list [label id & items]
-  (row label
-    [:div.btn-group {:field :multi-select :id id}
-      (for [[k label] items]
-        [:button.btn.btn-default {:key k} label])]))
-
-(def form
-  [:div
-   [:div.page-header [:h1 "Reagent Form"]]
-   (text-input :first-name "First name")
-   (text-input :last-name "Last name")
-   (selection-list "Favorite drinks" :favorite-drinks
-                   [:coffee "Coffee"]
-                   [:beer "Beer"]
-                   [:crab-juice "Crab juice"])])
-
-(defn save-doc [doc]
-  (fn []
-    (POST (str js/context "/save")
-          {:params {:doc @doc}
-           :handler (fn [_] (swap! state assoc :saved? true))})))
-
-(defn about []
-  [:div "this is the story of milkilo-tracker... work in progress"])
-
-(defn home []
-  (let [doc (atom {})]
-    (fn []
-      [:div
-       [bind-fields form doc
-        (fn [_ _ _] (swap! state assoc :saved? false) nil)]
-       (if (:saved? @state)
-         [:p "Saved"]
-         [:button {:type "submit"
-                   :class "btn btn-default"
-                   :onClick (save-doc doc)}
-          "Submit"])])))
 
 (defn navbar []
   [:div.navbar.navbar-inverse.navbar-fixed-top
@@ -61,23 +24,123 @@
     [:div.navbar-collapse.collapse
      [:ul.nav.navbar-nav
       [:li {:class (when (= home (:page @state)) "active")}
-       [:a {:on-click #(secretary/dispatch! "#/")} "Home"]]
+       [:a {:on-click #(secretary/dispatch! "#/")} "Dashboard"]]
+
+      [:li {:class (when (= add-entry (:page @state)) "active")}
+       [:a {:on-click #(secretary/dispatch! "#/add-entry")} "Lisää"]]
+
+      [:li {:class (when (= edit-entry (:page @state)) "active")}
+       [:a {:on-click #(secretary/dispatch! "#/entry/1")} "Muokkaa"]]
+
+      [:li {:class (when (= history (:page @state)) "active")}
+       [:a {:on-click #(secretary/dispatch! "#/history")} "Historia"]]
+
       [:li {:class (when (= about (:page @state)) "active")}
-       [:a {:on-click #(secretary/dispatch! "#/about")} "About"]]]]]])
+       [:a {:on-click #(secretary/dispatch! "#/about")} "Tietoja"]]]]]])
+
+(defn text-input [id label]
+  (row label [:input.form-control {:field :text :id id}]))
+
+(defn selection-list [label id & items]
+  (row label
+       [:div.btn-group {:field :single-select :id id}
+        (for [[k label] items]
+          [:button.btn.btn-default {:key k} label])]))
+
+(def form
+  [:div
+   [:h1 "Lisää uusi merkintä"]
+   (selection-list "Merkinnän tyyppi" :entry-type
+                   [:type-a "Tyyppi A"]
+                   [:type-b "Tyyppi B"]
+                   [:type-c "Tyyppi C"])
+   (text-input :value "Arvo")])
+
+(defn save-doc [doc]
+  (fn []
+    (POST (str js/context "/save")
+          {:params {:doc @doc}
+           :handler (fn [_] (swap! state assoc :saved? true))})))
+
+(defn about []
+  [:div
+   [:h1 "Tietoja sovelluksesta"]
+   [:p "todo"]])
+
+(defn history []
+  [:div
+   [:h1 "Historia"]
+   [:p "Add cool NVD3 diagrams and stuff"]]
+  )
+
+(defn add-entry []
+  (let [doc (atom {})]
+    (fn []
+      [:div
+       [bind-fields form doc
+        (fn [_ _ _] (swap! state assoc :saved? false) nil)]
+       (if (:saved? @state)
+         [:p "Saved"]
+         [:button {:type "submit"
+                   :class "btn btn-success"
+                   :onClick (save-doc doc)}
+          "Tallenna"])])))
+
+(defn edit-entry [entry-id]
+  [:div
+   [:h1 (str "Muokkaa merkintää " (@state :entry-id))]
+   [:button
+    {:class "btn btn-lg btn-success"
+     :on-click #(secretary/dispatch! "#/")} "Tallenna"]
+   [:button
+    {:class "btn btn-lg btn-warning"
+     :on-click #(secretary/dispatch! "#/")} "Peruuta"]
+   [:button
+    {:class "btn btn-lg btn-danger"
+     :on-click #(js/alert "TODO: remove")} "Poista"]
+   ]
+  )
+
+(defn home []
+  [:div
+   [:h1 "Dashboard"]
+   [:button
+    {:class "btn btn-lg btn-success"
+     :on-click #(secretary/dispatch! "#/add-entry")} "Lisää uusi merkintä"]
+   [:h2 "Entry list"]
+   [:ul
+    [:li [:a {:on-click #(secretary/dispatch! "#/entry/0")} "Add"]]
+    [:li [:a {:on-click #(secretary/dispatch! "#/entry/1")} "Some"]]
+    [:li [:a {:on-click #(secretary/dispatch! "#/entry/2")} "Entries"]]
+    [:li [:a {:on-click #(secretary/dispatch! "#/entry/3")} "Here"]]]])
 
 (defn page []
   [(:page @state)])
 
 (secretary/set-config! :prefix "#")
 
+(defn put! [k v]
+  (swap! state assoc k v))
+
 (defroute "/" []
-          (.log js/console "hi!")
-          (swap! state assoc :page home))
-(defroute "/about" [] (swap! state assoc :page about))
+  (.log js/console "hi!")
+  (put! :page home))
+
+(defroute "/entry/:id" {:as params}
+  (.log js/console (str "hi! " (params :id)))
+  (swap! state assoc :page edit-entry :entry-id (params :id)))
+
+(defroute "/add-entry" []
+  (.log js/console "Add entry-view")
+  (put! :page add-entry))
+
+(defroute "/history" []
+  (.log js/console "History-view")
+  (put! :page history))
+
+(defroute "/about" [] (put! :page about))
 
 (defn init! []
   (swap! state assoc :page home)
   (reagent/render-component [navbar] (.getElementById js/document "navbar"))
   (reagent/render-component [page] (.getElementById js/document "app")))
-
-
