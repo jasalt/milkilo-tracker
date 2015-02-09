c64e08d110046c9ccb1aeb4cb6b5f9e36d6b41ee
Jarkko Saltiola <jarkko.saltiola@gmail.com>
2015-02-09 20:31:55 +0200

    Secured client app. NOTE: some mystical going on.

--- src/milkilo_tracker/routes/home.clj
+++ src/milkilo_tracker/routes/home.clj
@@ -2,7 +2,8 @@
   (:require [milkilo-tracker.layout :as layout]
             [milkilo-tracker.util :as util]
             [compojure.core :refer :all]
-            [noir.response :refer [edn]]
+            [noir.response :refer [edn, redirect]]
+            [cemerick.friend :as friend]
             [clojure.pprint :refer [pprint]]))
 
 (def mock-entries
@@ -19,9 +20,20 @@
     }
    ])
 
+;; TODO Hackfixed authentication exception handling
+;; Why friends default unauth-handler does not catch error and redirect?
 (defn home-page []
-  (layout/render
-   "app.html"))
+  (try
+    (friend/authenticated (layout/render "app.html"))
+  (catch Exception e
+    (do
+      (println "GOT IT ++++++++++++++++")
+      (println (str "caught exception: " (.getMessage e)))
+      (redirect "/login")
+      )
+    )
+  )
+)
 
 (defn get-entries []
   (pprint "Return mock entries")
