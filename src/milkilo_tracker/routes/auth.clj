168246fba5202a590b02cee3dc24822d6325a2ba
Jarkko Saltiola <jarkko.saltiola@gmail.com>
2015-02-09 17:40:13 +0200

    Login working.

--- src/milkilo_tracker/routes/auth.clj
+++ src/milkilo_tracker/routes/auth.clj
@@ -1,29 +1,8 @@
 (ns milkilo-tracker.routes.auth
   (:require [milkilo-tracker.layout :as layout]
-            [milkilo-tracker.util :as util]
             [compojure.core :refer :all]
-            [noir.response :refer [edn]]
-            [clojure.pprint :refer [pprint]]
             [cemerick.friend :as friend]
             ))
-
-(def mock-entries
-  [
-   {:id 1
-    :value 2.55
-    :date "1.2.3123"
-    :type "A"
-    },
-   {:id 2
-    :value 35.31
-    :date "9.8.1229"
-    :type "B"
-    }
-   ])
-
-(defn login-page []
-  (layout/render
-   "login.html"))
 
 (defn login-page []
   (layout/render
@@ -31,5 +10,5 @@
 
 (defroutes auth-routes
   (GET "/login" [] (login-page))
-  (GET "/secret" [] (friend/authorize #{::user} "Logged in, it seems."))
+  (GET "/secret" [] (friend/authorize #{:milkilo-tracker.middleware/user} "Logged in, it seems."))
   )
