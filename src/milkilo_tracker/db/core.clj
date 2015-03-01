f07981f96759f1cddffd67a4f73439acef8df422
Jarkko Saltiola <jarkko.saltiola@gmail.com>
2015-03-01 19:32:31 +0200

    Clean nil values from entries db query.

--- src/milkilo_tracker/db/core.clj
+++ src/milkilo_tracker/db/core.clj
@@ -1,6 +1,7 @@
 (ns milkilo-tracker.db.core
   (:use korma.core
-        [korma.db :only (defdb)])
+        [korma.db :only (defdb)]
+        clojure.pprint)
   (:require [milkilo-tracker.db.schema :as schema]))
 
 ;; Create connection pool
@@ -33,16 +34,23 @@
             :results))
 
 (defn get-entries [site-id]
-  (map #(dissoc % :site_id)
-       (select entries
-               (where {:site_id site-id}))))
+  ;; Return entries by id. Site-id and nil attributes are removed from query.
+  (let [query-result (select entries
+                             (where {:site_id site-id}))
+        without-site-id (map #(dissoc % :site_id) query-result)
+
+        cleaned (map (fn [entry] (into {} (filter
+                                           (comp not nil? val) entry)))
+                     without-site-id)]
+    cleaned))
+;;(pprint (get-entries 1))
 
 (defn get-user-data [user-id]
+  ;; Get data of all users administered sites with entries.
   (let [user-sites (get-administered-sites user-id)
         cleaned-user-sites (map #(select-keys % [:id :name]) user-sites)]
     (map
      (fn [site]
        (merge site {:entries (get-entries (site :id))}))
      cleaned-user-sites)))
-
-;;(get-user-data 15)
+;;(pprint (get-user-data 3))
